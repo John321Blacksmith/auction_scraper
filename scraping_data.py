63289710be0b7f8fa9b1203d4fe6163c42c01777
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-08-04 22:58:28 +0300

    added site specs

---
+++ scraping_data.py
@@ -0,0 +1,15 @@
+auction_site = {
+	'source': 'https://nedradv.ru/nedradv/ru/auction',
+	'table': '.g-color-black-opacity-0_6',
+	'row': 'tr',
+	'link': 'a',
+	'object': '/html/body/main/section[3]/div/div[1]/div[1]',
+	'date': './/div[1]/h1',
+	'square': './/div[1]/p',
+	'region': './/div[1]/h1',
+	'status': './/dl[1]/dd',
+	'submit_deadline': './/dl[8]',
+	'contribution': './/dl[4]',
+	'organizer': './/dl[6]',
+	'next_page': ''
+}
