bd279758833967d12ced15e44e9321f8a6c519c9
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-08-04 22:59:31 +0300

    the Elememn

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import asyncio
 import requests_html
+from tools.scraping_tools import (WebManager, WebScraper, WebCrawler)
+
+from scraping_data import auction_site
 
 
+async def start_scraping():
+	scraper = WebScraper(auction_site)
+	links = await scraper.extract_table_row_link([auction_site['source']])
+	objects = await scraper.extract_data_of_each_item()
+	for i in range(0, len(objects)):
+		for key, value in objects.items():
+			print(key + ' ::: ' + str(value))
+
+		print('\n\n\n')
+	
 
 
 if __name__ == '__main__':
-	...
+	asyncio.run(start_scraping())
