d90a2902c3ad3e4327c7dc1979f99f55194f1b9b
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-08-04 12:52:29 +0300

    added model of the parsed item

---
+++ models.py
@@ -0,0 +1,8 @@
+class Auction:
+	date: int    # дата
+	square: str    # участок
+	area: str    # регион
+	status: str    # статус
+	submit_applications_deadline: int    # срок подачи заявок
+	contribution_for_auct_membership: int    # взнос за участие в аукционе
+	organizer: str    # организатор
