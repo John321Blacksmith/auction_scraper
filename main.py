6faf0d10c8b63c13e41c7b4507555bdf50607505
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-08-06 14:25:09 +0300

    all managers have been initialized

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
+import time
 import asyncio
 import requests_html
-from tools.scraping_tools import (WebManager, WebScraper, WebCrawler)
+from tools.data_tools import DataManager
+from tools.scraping_tools import (WebManager, WebScraper, WebCrawler, FileManager)
 
 from scraping_data import auction_site
 
 
-async def start_scraping():
-	scraper = WebScraper(auction_site)
-	links = await scraper.extract_table_row_link([auction_site['source']])
-	objects = await scraper.extract_data_of_each_item()
-	for i in range(0, len(objects)):
-		for key, value in objects.items():
-			print(key + ' ::: ' + str(value))
+file_manager = FileManager('links.csv')
 
-		print('\n\n\n')
+
+async def crawl_and_save_pages_links(filename):
+	"""
+	The first look at the website
+	and collecting the links in a
+	txt file.
+	"""
+	crawler = WebCrawler(filename, auction_site)
+	# gather links
+	await crawler.crawl_links(auction_site['source'])
+	# save them
+	await file_manager.save_links(crawler.list_of_page_links)
 	
 
+async def extract_auctions():
+	"""
+	Query the file with links
+	and use each link for request.
+	"""
+	# take links out 
+	links = await file_manager.pull_out_page_links()
 
+	scraper = WebScraper(auction_site)
+	# getting the row links
+	row_links = await scraper.extract_table_row_link(links)
+
+	# form auction object from each page
+	auctions = await scraper.exctract_data_of_each_item()
+
+	return auctions
+
+
+async def save_auctions():
+	auctions = await extract_auctions()
+	data_manager = DataManager()
+
+	
+# wait a little bit
+time.sleep(3)
 if __name__ == '__main__':
-	asyncio.run(start_scraping())
+	asyncio.run(main())
